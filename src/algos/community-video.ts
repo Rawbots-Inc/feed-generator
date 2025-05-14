c92dc474977e591d787446f915f50ebda66417af
Thanh Bui <thanhbpc.dev@gmail.com>
2025-05-14 23:19:17 +0700

    add community video feed

---
+++ src/algos/community-video.ts
@@ -0,0 +1,70 @@
+import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
+import { AppContext } from '../config'
+
+import { encodeCursor, decodeCursor } from '../util/helpers'
+
+export const shortname = 'community-video'
+
+export const handler = async (ctx: AppContext, params: QueryParams) => {
+  console.log(
+    `handler get feed generator with param: \n limit: ${params?.limit}\n cursor: ${params?.cursor}\nfeed: ${params?.feed}`,
+  )
+
+  let timestamp: number | null = null
+  let community: string | undefined
+  let oldCursor: string | undefined
+
+  if (params.cursor) {
+    try {
+      ;({ timestamp, community, oldCursor } = decodeCursor(params.cursor))
+      console.log(`Decoded: community=${community}, timestamp=${timestamp}`)
+    } catch (err) {
+      console.error('Failed to decode cursor:', err)
+    }
+  } else {
+    console.log('No cursor provided ⇒ list first page of all posts')
+  }
+
+  let builder = ctx.db.selectFrom('post').selectAll()
+
+  if (community) {
+    builder = builder.where('community', '=', community)
+    console.log(`Applying community filter: = ${community}`)
+  }
+
+  if (timestamp != null) {
+    const cutoff = new Date(timestamp).toISOString()
+    builder = builder.where('indexedAt', '<', cutoff)
+    console.log(`Applying cutoff: indexedAt < ${cutoff}`)
+  }
+
+  const rows = await builder
+    .orderBy('indexedAt', 'desc')
+    .orderBy('cid', 'desc')
+    .limit(params.limit ?? 10)
+    .execute()
+
+  const feed = rows.map((r) => ({ post: r.uri }))
+  console.log('Community feeds response', feed)
+
+  let newCursor: string | undefined
+  if (rows.length) {
+    const last = rows[rows.length - 1]
+    const newTs = new Date(last.indexedAt).getTime()
+    const restToken = oldCursor?.split('::', 2)[1] ?? ''
+    const newBase = `${newTs}::${restToken}`
+
+    if (community) {
+      const newCommEnc = Buffer.from(community, 'utf-8').toString('base64')
+      newCursor = `${newBase}_${newCommEnc}`
+    } else {
+      newCursor = newBase
+    }
+    console.log(`New cursor: ${newCursor}`)
+  }
+
+  return {
+    feed,
+    cursor: newCursor,
+  }
+}
--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,11 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as community from './community'
+import * as communityVideo from './community-video'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [community.shortname]: community.handler,
+  [communityVideo.shortname]: communityVideo.handler,
 }
 
 export default algos
